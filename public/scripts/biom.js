489bb88fdcf9e305fcdaaa96b0dc459932782785
Danika Rathnayake <danika.r@carehires.com>
2023-01-21 02:48:26 +0000

    fix: protein detail view added

--- public/scripts/biom.js
+++ public/scripts/biom.js
@@ -2,6 +2,7 @@ console.log("js file linked");
 
 const proteins = [
 {
+    id: '1',
     shortName: 'Cys C',
     commonName: 'Serum cystatin C',
     ncib: '9031',
@@ -9,6 +10,7 @@ const proteins = [
     coverImage: '../assets/images/bg-2.jpg'
 },
 {
+    id: '2',
     shortName: 'KIM 1',
     commonName: 'kidney injury molecule-1',
     ncib: '26762',
@@ -16,12 +18,18 @@ const proteins = [
     coverImage: '../assets/images/bg-1.jpg'
 },
 {
+    id: '3',
     shortName: 'IL-18',
     commonName: 'Interleukin 18',
     ncib: '8809',
     uniProtKB: 'Q13478',
     coverImage: '../assets/images/bg-3.jpg'
 }]
+
+function onClick(id){
+    window.location.href="/public/biom-detail.html";
+}
+
 const parent = document.getElementById('parent')
 
 if(parent) {
@@ -29,7 +37,7 @@ if(parent) {
         const div = document.createElement('div');
         div.className = 'col-3 mt-3';
 
-        let htmlString = `<div class="card biom-card rounded-4 overflow-hidden">
+        let htmlString = `<div class="card biom-card rounded-4 overflow-hidden" onclick="onClick('${protein.id}')">
                             <img class="image" src="${protein.coverImage}" >
                             <div class="content">
                                 <h5>${protein.shortName}</h5>
@@ -41,4 +49,6 @@ if(parent) {
     }
     
 }
+
+
 console.log(parent);
